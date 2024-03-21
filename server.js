daa91104ef3fc9ca25a1cf782022b6970450183b
nathan <106931831+Iceprod8@users.noreply.github.com>
2024-03-21 14:02:53 +0100

    Updat production

--- server.js
+++ server.js
@@ -9,27 +9,31 @@ const nextApp = next({ dev });
 const nextHandler = nextApp.getRequestHandler();
 
 nextApp.prepare().then(() => {
-  const server = createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    nextHandler(req, res, parsedUrl);
-  });
-
-  const io = socketIo(server);
-
-  io.on("connection", (socket) => {
-    socket.on("return_card", index => {
-        io.emit("return_card", index);
-    });
-      
-    socket.on("disconnect", () => {
-      console.log("Client disconnected");
+  if (!dev) {
+    const server = createServer((req, res) => {
+      const parsedUrl = parse(req.url, true);
+      nextHandler(req, res, parsedUrl);
     });
 
-    // Autres gestionnaires d"événements Socket.IO ici
-  });
+    const io = socketIo(server);
 
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
+    io.on("connection", (socket) => {
+      socket.on("return_card", index => {
+          io.emit("return_card", index);
+      });
+        
+      socket.on("disconnect", () => {
+        console.log("Client disconnected");
+      });
+  
+      // Autres gestionnaires d"événements Socket.IO ici
+    });
+
+    server.listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+  }
 });
+
+// En mode développement, exécuter simplement `next dev` séparément
